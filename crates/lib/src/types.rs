3197b704021696d0d983f2c253e7fc7177f8914f
Wojciech Polak <wp@impero.com>
2023-04-22 13:30:39 +0200

    Import static

--- crates/lib/src/types.rs
+++ crates/lib/src/types.rs
@@ -1,2 +1,24 @@
 // pub use crate::dyn_types::StaticTypeInfo as TypeInfo;
 // pub use crate::dyn_types::StaticItem as Item;
+
+pub use crate::heap_types::{
+    StaticAbi as Abi, StaticConstant as Constant, StaticCrate as Crate,
+    StaticDeprecation as Deprecation, StaticDiscriminant as Discriminant,
+    StaticDynTrait as DynTrait, StaticEnum as Enum, StaticExternalCrate as ExternalCrate,
+    StaticFnDecl as FnDecl, StaticFnInput as FnInput, StaticFunction as Function,
+    StaticFunctionPointer as FunctionPointer, StaticGenericArg as GenericArg,
+    StaticGenericArgs as GenericArgs, StaticGenericBound as GenericBound,
+    StaticGenericParamDef as GenericParamDef, StaticGenericParamDefKind as GenericParamDefKind,
+    StaticGenerics as Generics, StaticHeader as Header, StaticImpl as Impl, StaticImport as Import,
+    StaticItem as Item, StaticItemEnum as ItemEnum, StaticItemKind as ItemKind,
+    StaticItemSummary as ItemSummary, StaticMacroKind as MacroKind, StaticModule as Module,
+    StaticOpaqueTy as OpaqueTy, StaticPath as Path, StaticPolyTrait as PolyTrait,
+    StaticPrimitive as Primitive, StaticProcMacro as ProcMacro, StaticSpan as Span,
+    StaticStatic as Static, StaticStruct as Struct, StaticStructKind as StructKind,
+    StaticTerm as Term, StaticTrait as Trait, StaticTraitAlias as TraitAlias,
+    StaticTraitBoundModifier as TraitBoundModifier, StaticType as Type,
+    StaticTypeBinding as TypeBinding, StaticTypeBindingKind as TypeBindingKind,
+    StaticTypedef as Typedef, StaticUnion as Union, StaticVariant as Variant,
+    StaticVariantKind as VariantKind, StaticVisibility as Visibility,
+    StaticWherePredicate as WherePredicate,
+};
